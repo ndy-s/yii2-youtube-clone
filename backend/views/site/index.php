5007bc6495d7af447c23cf4f3f94ea2cb1b6beaa
Hendy <hendy.sg02@gmail.com>
2023-11-30 13:11:16 +0700

    Backend Dashboard Page

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -1,9 +1,57 @@
 <?php
 
 /** @var yii\web\View $this */
+/** @var $latestVideo \common\models\Video */
+/** @var $numberOfView integer */
+/** @var $numberOfSubscribers integer */
+/** @var $subscribers \common\models\Subscriber[] */
 
 $this->title = 'My Yii Application';
 ?>
-<div class="site-index">
-    index content goes here!
+<div class="site-index d-flex">
+    <div class="card m-2" style="width: 14rem;">
+        <div class="ratio ratio-16x9 mb-3">
+            <video src="<?php echo $latestVideo->getVideoLink() ?>"
+                   poster="<?php echo $latestVideo->getThumbnailLink() ?>"
+            ></video>
+        </div>
+        <div class="card-body">
+            <h6 class="card-title"><?php echo $latestVideo->title ?></h6>
+            <p class="card-text">
+                Likes: <?php echo $latestVideo->getLikes()->count() ?><br>
+                Views: <?php echo $latestVideo->getViews()->count() ?>
+            </p>
+            <a href="<?php echo \yii\helpers\Url::to(['/video/update', 'video_id' => $latestVideo->video_id]) ?>"
+               class="btn btn-primary"
+            >
+                Edit
+            </a>
+        </div>
+    </div>
+    <div class="card m-2" style="width: 14rem;">
+        <div class="card-body">
+            <h6 class="card-title">Total Views</h6>
+            <p class="card-text" style="font-size: 72px">
+                <?php echo $numberOfView ?>
+            </p>
+        </div>
+    </div>
+    <div class="card m-2" style="width: 14rem;">
+        <div class="card-body">
+            <h6 class="card-title">Total Subscribers</h6>
+            <p class="card-text" style="font-size: 72px">
+                <?php echo $numberOfSubscribers ?>
+            </p>
+        </div>
+    </div>
+    <div class="card m-2" style="width: 14rem;">
+        <div class="card-body">
+            <h6 class="card-title">Latest Subscribers</h6>
+            <?php foreach ($subscribers as $subscriber): ?>
+                <div>
+                    <?php echo $subscriber->user->username ?>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
 </div>
